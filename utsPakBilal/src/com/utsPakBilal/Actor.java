af58ae73dae5ee660a9d48378481ff91654b8852
virnandadwita19 <30596080+virnandadwita19@users.noreply.github.com>
2017-11-19 06:25:00 +0700

    Mengupload Code
    Mengupload code Actor.java

--- utsPakBilal/src/com/utsPakBilal/Actor.java
+++ utsPakBilal/src/com/utsPakBilal/Actor.java
@@ -4,5 +4,30 @@ package com.utsPakBilal;
 */
 
 public class Actor extends Sprite{
-	// Code dimulai disini.
+	//Equip hero.
+    public String equip_Head, equip_Body, equip_Weapon, equip_Acc;
+
+
+    //Function Kenalan
+    public void heroKenalan(){
+        System.out.println("Hallo nama saya " + getSprite_Name() +"\n"+
+                "Saya seorang " + getSprite_Job() + "\n\n");
+    }
+
+    //Function Lari
+    public void heroLari(){
+        System.out.println("Saya lari..!\nKearah sesuai arah jalan." + "\n\n");
+    }
+
+    public void heroBerjalan(String arahJalan) {
+        System.out.println("Hero berjalan ke-arah " + arahJalan + "\n\n");
+    }
+
+    public void heroLompat(int kaliLompat){
+        System.out.println("Saya lompat.");
+        for (int lompat=0; lompat<kaliLompat; lompat++){
+            System.out.println("Hup");
+        }
+        System.out.println("\n\n");
+    }
 }
